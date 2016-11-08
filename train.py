2c4549511bc0cd8cc7c281ac1503977dd5aa93e1
mjpekala <mike.pekala@jhuapl.edu>
2016-11-08 14:09:18 -0500

    moved some code into the new data_tools module

--- train.py
+++ train.py
@@ -2,7 +2,6 @@
    Trains a dense (per-pixel) classifier.
 """
 
-
 from __future__ import print_function
 
 __author__ = 'mjp, Oct 2016'
@@ -12,7 +11,6 @@ __license__ = 'Apache 2.0'
 
 import os, sys
 import pdb
-from PIL import Image
 
 import numpy as np
 from scipy.interpolate import interp2d
@@ -25,47 +23,9 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as K
 
+from data_tools import *
 
-#-------------------------------------------------------------------------------
-# Utility/helper functions
-#-------------------------------------------------------------------------------
 
-def load_multilayer_tiff(data_file):
-    """ Loads data from a grayscale multilayer .tif file.
-
-    data_file    := the tiff file name
-    add_feat_dim := adds a new "feature dimensinon" to grayscale data
-
-    RETURNS:
-       A numpy tensor with dimensions:
-           (#images, width, height)
-       -or-
-           (#images, 1, width, height)
-    """
-    if not os.path.isfile(data_file):
-        raise RuntimeError('could not find file "%s"' % data_file)
-    
-    # load the data from multi-layer TIF files
-    img = Image.open(data_file)
-    X = [];
-    for ii in xrange(sys.maxint):
-        Xi = np.array(img, dtype=np.float32)
-        if Xi.ndim == 2:
-            Xi = Xi[np.newaxis, ...] # add slice dimension
-        X.append(Xi)
-        try:
-            img.seek(img.tell()+1)
-        except EOFError:
-            break # this just means hit end of file (not really an error)
-
-    # list of images -> 3d tensor
-    X = np.concatenate(X, axis=0) 
-
-    # add a channel dimension 
-    if X.ndim == 3:
-        X = X[:, np.newaxis, :, :]
-        
-    return X
 
 
 def minibatch_indices(n_examples, mb_size=10):
@@ -77,122 +37,6 @@ def minibatch_indices(n_examples, mb_size=10):
         yield idx[ii:ii+n_this_batch]
 
 
-def apply_symmetries(X):
-    """Implements synthetic data augmentation by randomly appling
-    an element of the group of symmetries of the square to a single 
-    mini-batch of data.
-
-    The default set of data augmentation operations correspond to
-    the symmetries of the square (a non abelian group).  The
-    elements of this group are:
-
-      o four rotations (0, pi/2, pi, 3*pi/4)
-        Denote these by: R0 R1 R2 R3
-
-      o two mirror images (about y-axis or x-axis)
-        Denote these by: M1 M2
-
-      o two diagonal flips (about y=-x or y=x)
-        Denote these by: D1 D2
-
-    This page has a nice visual depiction:
-      http://www.cs.umb.edu/~eb/d4/
-
-
-    Parameters: 
-       X := Mini-batch data (#examples, #channels, rows, colums) 
-    """
-
-    def R0(X):
-        return X  # this is the identity map
-
-    def M1(X):
-        return X[:,:,::-1,:]
-
-    def M2(X): 
-        return X[:,:,:,::-1]
-
-    def D1(X):
-        return np.transpose(X, [0, 1, 3, 2])
-
-    def R1(X):
-        return D1(M2(X))   # = rot90 on the last two dimensions
-
-    def R2(X):
-        return M2(M1(X))
-
-    def R3(X): 
-        return D2(M2(X))
-
-    def D2(X):
-        return R1(M1(X))
-
-
-    symmetries = [R0, R1, R2, R3, M1, M2, D1, D2]
-    op = np.random.choice(symmetries) 
-        
-    # For some reason, the implementation of row and column reversals, 
-    #     e.g.      X[:,:,::-1,:]
-    # break PyCaffe.  Numpy must be doing something under the hood 
-    # (e.g. changing from C order to Fortran order) to implement this 
-    # efficiently which is incompatible w/ PyCaffe.  
-    # Hence the explicit construction of X2 with order 'C' below.
-    #
-    # Not sure this matters for Theano/Keras, but leave in place anyway.
-    X2 = np.zeros(X.shape, dtype=np.float32, order='C') 
-    X2[...] = op(X)
-
-    return X2
-
-
-
-
-def apply_warping(Z, scale=2):
-    return Z # the code below is broken
-
-    # TODO: try PIL.Image transform (see warp argument)
-    
-    assert(Z.ndim == 2)
-
-    # indices of the original lattice upon which Z lives
-    x = np.arange(Z.shape[0])
-    y = np.arange(Z.shape[1])
-
-    xx, yy = np.meshgrid(x,y) 
-
-    # perturbations to the lattice
-    m = int(np.ceil(Z.shape[0] / 3.))
-    n = int(np.ceil(Z.shape[1] / 3.))
-
-    dx = scale * np.random.rand(3,3)
-    dx = dx * np.array([[-1, 0, 1],
-                        [-1, 0, 1],
-                        [-1, 0, 1]])
-    dx = np.kron(dx, np.ones((m,n)))
-    dx = dx[0:Z.shape[0], 0:Z.shape[1]]  # truncate
-
-    dy = scale * np.random.rand(3,3)
-    dy = dy * np.array([[1, 1, 1],
-                        [0, 0, 0],
-                        [-1, -1, -1]])
-    dy = np.kron(dy, np.ones((m,n)))
-    dy = dy[0:Z.shape[0], 0:Z.shape[1]]  # truncate
-
-    # perturbed locations
-    xx_d = xx + dx
-    yy_d = yy + dy
-
-    # interpolated result
-    f_interp = interp2d(xx_d, yy_d, Z, kind='cubic');
-
-    Z_out = f_interp(x, y)
-
-    return Z_out
-
-
-#-------------------------------------------------------------------------------
-# CNN functions
-#-------------------------------------------------------------------------------
 
 def f1_score(y_true, y_hat):
     """ Note: this works for keras objects (e.g. during training) or 
