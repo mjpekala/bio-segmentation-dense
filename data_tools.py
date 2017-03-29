ec592052d18ce2aa7c5fb420fb23268ce869ea6e
mjpekala <mike.pekala@jhuapl.edu>
2017-03-29 16:49:33 -0400

    changed data type of y_one_hot from bool to int (for Keras)

--- data_tools.py
+++ data_tools.py
@@ -84,7 +84,7 @@ def pixelwise_one_hot(Y):
     """
     assert(Y.shape[1] == 1)
     n_classes = np.floor(np.max(Y) + 1).astype(np.uint32)
-    Y_onehot = np.zeros((Y.shape[0], n_classes, Y.shape[2], Y.shape[3]), dtype=np.bool)
+    Y_onehot = np.zeros((Y.shape[0], n_classes, Y.shape[2], Y.shape[3]), dtype=np.int32)
     for yi in np.arange(n_classes):
         Y_onehot[:,yi,:,:] = np.squeeze(Y == yi)
     return Y_onehot
