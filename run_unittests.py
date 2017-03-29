be84a80450d89a5a2e82a1d6294d1a7162ba5b9b
mjpekala <mike.pekala@jhuapl.edu>
2017-03-29 12:07:46 -0400

    added a new test

--- run_unittests.py
+++ run_unittests.py
@@ -8,7 +8,7 @@ import unittest
 import numpy as np
 
 from data_tools import *
-from train import f1_score
+from cnn_tools import f1_score
 
 
 
@@ -21,6 +21,20 @@ class TestStuff(unittest.TestCase):
         y_hat[0:50] = 0
         self.assertTrue(np.abs(f1_score(y_true, y_hat) - 2./3) < 1e-8)
 
+
+    def test_y_onehot(self):
+        y_fake = np.random.randint(low=0, high=10, size=(5,1,3,3))
+        y_onehot = pixelwise_one_hot(y_fake)
+        
+        self.assertTrue(y_onehot.shape[1] == 10)
+
+        for kk in range(y_fake.shape[0]):
+            for ii in range(y_fake.shape[2]):
+                for jj in range(y_fake.shape[3]):
+                    yi = y_fake[kk,0,ii,jj]
+                    self.assertTrue(y_onehot[kk,yi,ii,jj])
+                    self.assertTrue(np.sum(y_onehot[kk,:,ii,jj]) == 1)
+                    
         
 
 
