5cfcbf8b6d44a6ef75278180fe89e1b9ab361637
mjpekala <mike.pekala@jhuapl.edu>
2017-03-30 15:31:34 -0400

    tweaked custom softmax for (hopefully) more numerical stability

--- cnn_tools.py
+++ cnn_tools.py
@@ -182,7 +182,9 @@ def create_unet(sz, n_classes=2, multi_label=False):
     def custom_softmax(x):
         # subtracting the max for numerical stability
         e_x = K.exp(x - K.max(x, axis=1, keepdims=True))
-        softmax = e_x / K.sum(e_x, axis=1, keepdims=True)
+        total = K.sum(e_x, axis=1, keepdims=True) + 1e-6
+        softmax = e_x / total
+        #softmax = e_x / (K.sum(e_x, axis=1, keepdims=True) + 1e-8)
         return softmax
 
     if multi_label: 
