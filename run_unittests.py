cb5f673165b69204d0375bebc7cede04bf4121ca
mjpekala <mike.pekala@jhuapl.edu>
2017-05-05 12:36:00 -0400

    updated to exercise missing class labels case for pixelwise_ace_loss

--- run_unittests.py
+++ run_unittests.py
@@ -8,13 +8,14 @@ import unittest
 import numpy as np
 
 from data_tools import pixelwise_one_hot
-from cnn_tools import f1_score
+from cnn_tools import f1_score, pixelwise_ace_loss
 
 import theano
 
 
 
 class TestStuff(unittest.TestCase):
+    
     def test_f1_score(self):
         n_examples = 100
         n_classes = 2
@@ -34,10 +35,12 @@ class TestStuff(unittest.TestCase):
         result = f_theano.eval({a : y_true, b : y_true})
         self.assertTrue(np.abs(result - 1.0) < 1e-12)
 
+        # case where all estimates are incorrect
         y_totally_wrong = 1 - y_true
         result = f_theano.eval({a : y_true, b : 1.0 - y_true})
         self.assertTrue(np.abs(result - 0.0) < 1e-12)
 
+        # kill recall performance by 1/2
         y_recall_half = np.copy(y_true)
         y_recall_half[:,:,:,0:n4] =  1 - y_recall_half[:,:,:,0:n4]
         expected = 2./3      # 2 * (.5 * 1) / (.5 + 1)
@@ -49,7 +52,7 @@ class TestStuff(unittest.TestCase):
     def test_y_onehot(self):
         y_fake = np.random.randint(low=0, high=10, size=(5,1,3,3))
         y_onehot = pixelwise_one_hot(y_fake)
-        
+ 
         self.assertTrue(y_onehot.shape[1] == 10)
 
         for kk in range(y_fake.shape[0]):
@@ -60,7 +63,75 @@ class TestStuff(unittest.TestCase):
                     self.assertTrue(np.sum(y_onehot[kk,:,ii,jj]) == 1)
 
         # make sure missing labels work as expected
+        y_fake = np.random.randint(low=0, high=10, size=(5,1,3,3))
+        y_fake[0,:,:,:] = -10
+        y_onehot = pixelwise_one_hot(y_fake)
+        assert(np.all(y_onehot[0,:,:,:] == 0))
+
         
+    def test_pixelwise_ace_loss(self):
+        n_examples = 100
+        n_classes = 5
+        m,n = 16, 16    # num pixels
+        n2 = np.floor(n/2).astype(np.int32)
+        n4 = np.floor(n/4).astype(np.int32)
+
+        y_true_raw = np.random.randint(low=0, high=n_classes, size=(n_examples, 1, m, n))
+        y_true = pixelwise_one_hot(y_true_raw).astype(np.float32)
+
+        # Note: we assume theano backend here
+        a = theano.tensor.tensor4('y_true')
+        b = theano.tensor.tensor4('y_hat')
+        f_theano = pixelwise_ace_loss(a,b)
+
+        #--------------------------------------------------
+        # perfect estimate 
+        #--------------------------------------------------
+        lossPerfect = f_theano.eval({a : y_true, b : y_true})
+        self.assertTrue(lossPerfect < 0)
+
+        #--------------------------------------------------
+        # no labels \equiv perfect estimate
+        #--------------------------------------------------
+        lossNL = f_theano.eval({a : np.zeros(y_true.shape, dtype=np.float32), b : y_true})
+        print(lossNL)
+        self.assertTrue(np.abs(lossNL - lossPerfect) < 1e-9)
+
+        #--------------------------------------------------
+        # make some mistakes and see the loss is higher
+        #--------------------------------------------------
+        y_hat = np.copy(y_true)
+        splatter = np.random.randint(low=0, high=y_hat.size, size=(500,))
+        tmp = y_hat.ravel()
+        tmp[splatter] = 1
+        y_hat = np.reshape(tmp, y_hat.shape)
+        lossErr = f_theano.eval({a : y_true, b : y_hat})
+        self.assertTrue(lossPerfect < lossErr)
+
+        #--------------------------------------------------
+        # make sure that "unlabeled" pixels do not contribute to error
+        #--------------------------------------------------
+        supp = np.random.randint(low=0, high=y_true_raw.size, size=(500,))
+        y_true_u = np.copy(y_true_raw).flatten()
+        y_true_u[supp] = -1
+
+        y_hat = np.copy(y_true_u).flatten()
+        y_hat[supp] = np.mod(y_true_u[supp] + 1, n_classes)  # all in error
+        
+        y_true_u = np.reshape(y_true_u, y_true_raw.shape)
+        y_hat = np.reshape(y_hat, y_true_u.shape)
+        
+        y_true_u = pixelwise_one_hot(y_true_u).astype(np.float32)
+        y_hat = pixelwise_one_hot(y_hat).astype(np.float32)
+        
+        loss1 = f_theano.eval({a : y_true, b : y_hat})
+        loss2 = f_theano.eval({a : y_true_u, b : y_hat})
+        self.assertTrue(loss2 < loss1)
+        
+
+
+
+
 
 
 if __name__ == '__main__':
