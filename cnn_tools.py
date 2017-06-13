47779390119b2e86eea8b5e858cd108e6c704c62
mjpekala <mike.pekala@jhuapl.edu>
2017-06-13 11:37:54 -0400

    added a print statement

--- cnn_tools.py
+++ cnn_tools.py
@@ -300,6 +300,7 @@ def train_model(X_train, Y_train, X_valid, Y_valid, model,
     print('[train_model]: Y_train is ', Y_train.shape, Y_train.dtype, np.min(Y_train), np.max(Y_train))
     print('[train_model]: X_valid is ', X_valid.shape, X_valid.dtype, np.min(X_valid), np.max(X_valid))
     print('[train_model]: Y_valid is ', Y_valid.shape, Y_valid.dtype, np.min(Y_valid), np.max(Y_valid))
+    print('[train_model]: model input shape: ', model.input_shape)
 
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
